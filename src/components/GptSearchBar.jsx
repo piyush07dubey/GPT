a1961889664e57d28d74bb948af3b89159a1c1eb
piyush <you@example.com>
2026-05-04 00:23:47 +0530

    make searh functionality

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,18 +1,40 @@
 import React, { useRef } from 'react'
 import lang from '../utils/languageConstants'
-import {useSelector} from 'react-redux'
-import {groq} from "../utils/Groq"
+import {useSelector,useDispatch} from 'react-redux'
+import { addGptMovieResult } from '../utils/gptSlice'
+
 
 const GptSearchBar = () => {
     const langKey=useSelector((store)=>store.config.lang)
     const searchText=useRef(null)
-    const handleGptSearchClick=async()=>{
-        const gptResults = await groq.chat.completions.create({
-  messages: [{ role: "user", content: getQuery }],
-  model: "llama-3.1-8b-instant",
-});  
+    const dispatch=useDispatch();
+    const searchMovieTMDB=async(movie)=>{
+       const data=await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`,API_OPTIONS)
+       const json=await data.json();
+       return json.results;
     }
-    const getQuery="Act as a movie Recommendation system and suggest some movies for the query"+searchText.current.value+"only give me names of 5 movies,comma seperated  like the example result given ahead,Example Result:Sholay,Gadar,Golmaal,koi mil gaya"
+    const handleGptSearchClick = async () => {
+    // 1. Define the query inside the click handler to get the latest value
+    const getQuery = "Act as a movie Recommendation system and suggest some movies for the query: " + 
+                     searchText.current.value + 
+                     ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Sholay, Gadar, Golmaal, Koi Mil Gaya, Don";
+
+    // 2. Get GPT/Groq results
+    const gptResults = await groq.chat.completions.create({
+        messages: [{ role: "user", content: getQuery }],
+        model: "llama-3.1-8b-instant",
+    });
+
+    // 3. Extract the movie names string
+    const getMovies = gptResults.choices?.[0]?.message?.content;
+    if (!getMovies) return;
+
+    const promiseArray = getMovies.map(movie => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+   dispatch(addGptMovieResult({movieNames:getMovies,movieResults:tmdbResults}))
+};
   return (
     <div className='pt-[20%] flex justify-center'>
         <form className='w-1/2 bg-white grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
